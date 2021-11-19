3c1d75c98a5b3dd5e92715ef91e65e3c2d843c16
John Denny <johnnydenny@gmail.com>
2021-11-19 02:25:50 -0500

    added UI using JavaScript events

--- script.js
+++ script.js
@@ -1,74 +1,86 @@
+window.onload = function() {
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    if (roundNum > 5) {
+            return;
+        } 
+        playerSelection = button.value;
+        compSelection = computerPlay();
+        playRound(playerSelection, compSelection);
+  });
+});
+
+}
+
+let playerScore = 0;
+let compScore = 0;
+let roundNum = 1;
+
 function computerPlay() {
     const myChoices = ['rock', 'paper', 'scissors'];
     return myChoices[Math.floor(Math.random()*myChoices.length)]    
 }
 
-let playerScore = 0;
-let computerScore = 0;
-let roundNum = 1;
-let computerSelection = computerPlay();
-
 function playRound(playerSelection, computerSelection) {
     
-    if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return "lose"
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return "lose"
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return "lose"
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return "win"
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return "win"
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return "win"
-    } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
-        return "tie"
-    }  else if (playerSelection == 'rock' && computerSelection == 'rock') {
-        return "tie"
-    } else if (playerSelection == 'paper' && computerSelection == 'paper') {
-        return "tie"
-    } else {
-        return "invalid"
+    if (playerSelection == 'rock' && computerSelection == 'paper' || 
+        playerSelection == 'paper' && computerSelection == 'scissors' || 
+        playerSelection == 'scissors' && computerSelection == 'rock') {
+        roundNum += 1;
+        compScore += 1;
+        roundResults('lose');
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors' || 
+        playerSelection == 'paper' && computerSelection == 'rock' || 
+        playerSelection == 'scissors' && computerSelection == 'paper') {
+        roundNum += 1;
+        playerScore += 1;
+        roundResults('win');
+    } else if (playerSelection == 'scissors' && computerSelection == 'scissors' || 
+        playerSelection == 'rock' && computerSelection == 'rock' || 
+        playerSelection == 'paper' && computerSelection == 'paper') {
+        roundNum += 1;
+        roundResults('tie');
+    }  else {
+        roundResults('invalid');
     }
 }
 
-function game() {
-    playerInput = prompt("Round " + roundNum + ": " + "Rock, Paper or Scissors");
-    const playerSelection =  playerInput.toLowerCase();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    playRound(playerSelection, computerSelection); {
-        if (playRound(playerSelection, computerSelection) === "win" ) {
-            playerScore += 1; roundNum += 1
-            return alert("You Win!")
-        } else if (playRound(playerSelection, computerSelection) === "lose") {
-            computerScore += 1; roundNum += 1
-            return alert("You Lose!")
-        } else if (playRound(playerSelection, computerSelection) === "tie") {
-            playerScore += 0; computerScore += 0; roundNum += 1
-            return alert("You Tie!")
-        } else if (playRound(playerSelection, computerSelection) === "invalid")
-            return alert("Wrong Choice! Check your spelling and try again!"), game();
+function roundResults(result) {
+    if (result === 'lose') {
+        resultsEvents(`You Lost. You chose ${playerSelection}. The Computer chose ${compSelection}. The Score is Computer: ${compScore} Human: ${playerScore}.`);
+        finalScore(roundNum);
+    } else if (result === 'win') {
+        resultsEvents(`You Won! You chose ${playerSelection}. The Computer chose ${compSelection}. The Score is Computer: ${compScore} Human: ${playerScore}.`);
+        finalScore(roundNum);
+    } else if (result === 'tie') {
+        resultsEvents(`You Tied. You chose ${playerSelection}. The Computer chose ${compSelection}. The Score is Computer: ${compScore} Human: ${playerScore}.`);
+        finalScore(roundNum);
+    } else {
+        resultsEvents('INVALID');
+        finalScore(roundNum);
     }
 }
 
-function finalScore() {
-    if (playerScore > computerScore) {
-        return alert("Final Score: You won " + playerScore + " games out of 5! You Win!" )
-    } else if (playerScore < computerScore) {
-        return alert("Final Score: You lost " + computerScore + " games out of 5! You Lose!")
-    } else {
-        return alert("Final Score: You both won " + playerScore + " games out of 5! You Tied!")
-    } 
+function finalScore(rounds) {
+    if (rounds > 5 && playerScore > compScore) {
+        resultsEvents(`Final Score = Computer: ${compScore} Player: ${playerScore}. You Won!`);
+    } else if (rounds > 5 && playerScore < compScore) {
+        resultsEvents(`Final Score = Computer: ${compScore} Player: ${playerScore}. You Lost.`);
+    } else if (rounds > 5 && playerScore === compScore) {
+        resultsEvents(`Final Score = Computer: ${compScore} Player: ${playerScore}. You Tied.`);
+    }
 }
 
-alert("Try and Win Rock, Paper, Scissors in 5 Rounds");
-game();
-game();
-game();
-game();
-game();
-finalScore();
-console.log(playerScore, computerScore);
+function resultsEvents(text) {
+    const resultsContainer = document.querySelector('.results');
+        const resultspara = document.createElement('p');
+        resultspara.textContent = text;
+        resultsContainer.appendChild(resultspara);
+}
